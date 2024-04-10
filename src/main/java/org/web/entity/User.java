703db4cce0ea53e1dc58a251f41925fd8403deb1
Yurii Mamichenko <yuramamichenko@gmail.com>
2024-04-10 09:55:35 +0300

    Create User Class

---
+++ src/main/java/org/web/entity/User.java
@@ -0,0 +1,25 @@
+package org.web.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    // standard constructors / setters / getters / toString
+}
